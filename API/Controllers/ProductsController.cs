cf31d693afacf943cbc3a87d660f56b96767eedb
wadpas <wadpas@gmail.com>
2023-06-27 09:42:40 +0300

    Configurated Swagger

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,6 +7,8 @@ using Core.Specifications;
 using API.Dtos;
 using System.Linq;
 using AutoMapper;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
 
 namespace API.Controllers
 {
@@ -42,11 +44,15 @@ namespace API.Controllers
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     { 
       var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
       var product = await _productsRepo.GetEntityWithSpec(spec);
+
+      if (product == null) return NotFound(new ApiResponse(404));
 
       return _mapper.Map<Product, ProductToReturnDto>(product);
     }
